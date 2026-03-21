d2f750b5141cdc9c10cae5c94c61568d5cf2805c
kexo <cassio_vittori@hotmail.com>
2026-03-20 22:02:41 -0300

    add class Var, Decl, Programa

--- ast_nodes.py
+++ ast_nodes.py
@@ -1,4 +1,3 @@
-
 class Number:
     def __init__(self, value):
         self.value = int(value)
@@ -9,3 +8,16 @@ class BinOp:
         self.op = op
         self.right = right
 
+class Var:
+    def __init__(self, name):
+        self.name = name
+
+class Decl:
+    def __init__(self, name, expr):
+        self.name = name
+        self.expr = expr
+
+class Programa:
+    def __init__(self, decls, result):
+        self.decls = decls
+        self.result = result
