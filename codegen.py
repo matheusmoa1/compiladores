2a4de2641df67622d7bddae13742474bd4ec747f
kexo <cassio_vittori@hotmail.com>
2026-03-20 22:04:43 -0300

    Gera BSS e codigo para variaveis

--- codegen.py
+++ codegen.py
@@ -1,14 +1,18 @@
-def generate(node):
-    if node.__class__.__name__ == "Number":
+from ast_nodes import Number, BinOp, Var, Decl, Programa
+
+def generate_expr(node):
+    if isinstance(node, Number):
         return f"    mov ${node.value}, %rax\n"
 
-    elif node.__class__.__name__ == "BinOp":
-        code = ""
-        code += generate(node.right)
-        code += "    push %rax\n"
-        code += generate(node.left)
-        code += "    pop %rbx\n"
+    elif isinstance(node, Var):
+        return f"    mov {node.name}(%rip), %rax\n"
 
+    elif isinstance(node, BinOp):
+        code = ""
+        code += generate_expr(node.right)
+        code += "    push %rax\n"
+        code += generate_expr(node.left)
+        code += "    pop %rbx\n"
         if node.op == "+":
             code += "    add %rbx, %rax\n"
         elif node.op == "-":
@@ -19,3 +23,19 @@ def generate(node):
             code += "    cqo\n"
             code += "    idiv %rbx\n"
         return code
+
+    else:
+        raise ValueError(f"No desconhecido em generate_expr: {node}")
+
+def generate(programa):
+    bss = ".section .bss\n"
+    for decl in programa.decls:
+        bss += f"    .lcomm {decl.name}, 8\n"
+
+    code = ""
+    for decl in programa.decls:
+        code += generate_expr(decl.expr)
+        code += f"    mov %rax, {decl.name}(%rip)\n"
+
+    code += generate_expr(programa.result)
+    return bss, code
